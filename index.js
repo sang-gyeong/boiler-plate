7fbd63dcadf775a1428467d746edc19b53401c57
이상경 <sknglee22@naver.com>
2020-08-26 11:11:09 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const User = require('./models/User')
+const config = require('./config/key')
+
+//*bodyparser 옵션 설정*
+//application/x-www-form-urlencoded이걸 분석해줌
+app.use(bodyParser.urlencoded({ extended: true }))
+    //application/json을 분석해줌
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://gyeong:sktkdrud2@boilerplate.l648y.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
@@ -12,5 +21,22 @@ mongoose.connect('mongodb+srv://gyeong:sktkdrud2@boilerplate.l648y.mongodb.net/<
     .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req, res) => {
+    //회원 가입할때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터베이스에 넣어준다.
+
+    //유저모델 인스턴스 생성
+    const user = new User(req.body);
+    //req.body안에는 json형식으로 id, password등이 들어있음.
+    //이는 앞의 bodyParser덕분
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err }) //성공하지 못하면 제이슨형식으로 알려줌
+        return res.status(200).json({ //성공시
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on part ${port}!`))
